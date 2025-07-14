e884c8c548d2b9f41984a1e16a520a5393e99134
bhumit08 <bhumitthumar115@gmail.com>
2025-07-14 10:24:45 +0530

    final version of weather app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,52 +1,43 @@
-import React, { useEffect,useRef, useState } from 'react'
-import './Weather.css'
-import search_icon from '../assets/search.png'
-// import clear_icon from '../assets/clear.png'
-// import cloud_icon from '../assets/cloud.png'
-// import drizzle_icon from '../assets/drizzle.png'
-// import rain_icon from '../assets/rain.png'
-// import snow_icon from '../assets/snow.png'
-import wind_icon from '../assets/wind.png'
-import humidity_icon from '../assets/humidity.png'
-
+import React, { useEffect, useRef, useState } from 'react';
+import './Weather.css';
+import search_icon from '../assets/search.png';
+import wind_icon from '../assets/wind.png';
+import humidity_icon from '../assets/humidity.png';
 
 function Weather() {
-  const inputRef=useRef()
-  const[weatherData,setWeatherData]=useState(false);
+  const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState(null);
 
-
-  const search=async (city)=>{
-    if(city===""){
-      alert("Enter city name");
+  const search = async (city) => {
+    if (city === '') {
+      alert('Enter city name');
       return;
     }
-    try{
-      const url=`https://api.weatherapi.com/v1/current.json?key=7325e80c95144cd69a9180025243012&q=${city}&aqcity`;
-
-      const response= await fetch(url);
-      const data= await response.json();
-      console.log(data);
-      setWeatherData({
-        humidity:data.current.humidity,
-        windspeed:data.current.wind_kph,
-        temperature:Math.floor(data.current.temp_c),
-        location:data.location.name,
-        img:data.current.condition.icon
-      })
-
-    }catch(erro){
-
+    try {
+      const url = `https://api.weatherapi.com/v1/current.json?key=7325e80c95144cd69a9180025243012&q=${city}&aqi=yes`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeatherData(data);
+    } catch (error) {
+      console.error('Error fetching weather:', error);
     }
-  }
-    useEffect(()=>{
-        search("gandhinagar");
-    },[])
+  };
+
+  useEffect(() => {
+    search('Gandhinagar');
+  }, []);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      search(inputRef.current.value);
+    }
+  };
 
   return (
-    <div className='weather'>
-      <div className='search-bar'>
-        <input ref={inputRef} type="text"placeholder='search'/>
-        <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
+    <div className="weather">
+      <div className="search-bar">
+        <input ref={inputRef} type="text" placeholder="Search City" onKeyDown={handleKeyDown} />
+        <img src={search_icon} alt="Search" onClick={() => search(inputRef.current.value)} />
       </div>
 
       {weatherData && weatherData.current && (
@@ -55,27 +46,50 @@ function Weather() {
           <p className="temperature">{weatherData.current.temp_c}°C</p>
           <p className="location">{weatherData.location.name}, {weatherData.location.country}</p>
 
-      <div className='weather-data'>
+          <div className="weather-data">
 
-        <div className="col">
-          <img src={humidity_icon} alt=""/>
-          <div>
-            <p>{weatherData.humidity}%</p>
-            <span>humidity</span>
+            <div className="col">
+              <img src={humidity_icon} alt="Humidity" />
+              <div>
+                <p>{weatherData.current.humidity}%</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+
+            <div className="col">
+              <img src={wind_icon} alt="Wind" />
+              <div>
+                <p>{weatherData.current.wind_kph} km/h</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
+
+            <div className="col">
+              <div>
+                <p>{weatherData.current.feelslike_c}°C</p>
+                <span>Feels Like</span>
+              </div>
+            </div>
+
+            <div className="col">
+              <div>
+                <p>{weatherData.current.uv}</p>
+                <span>UV Index</span>
+              </div>
+            </div>
+
+            <div className="col">
+              <div>
+                <p>{weatherData.current.vis_km} km</p>
+                <span>Visibility</span>
+              </div>
+            </div>
+
           </div>
-        </div>
-
-        <div className="col">
-          <img src={wind_icon} alt=""/>
-          <div>
-            <p>{weatherData.windspeed}km/h</p>
-            <span>Wind Speed</span>
-          </div>
-        </div>
-
-      </div>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Weather
+export default Weather;
